fce385141eb0ea0ed46279ec2adb11909dcc9a50
23jy0121 <23jy0121@jec.ac.jp>
2024-10-16 15:29:43 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public static class Program {
+class Program {
   public static void main(String[] args){
     System.out.println("This is test commit 1");
     System.out.println("This is test commit 2");
