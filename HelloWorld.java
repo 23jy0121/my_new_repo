a2be0c47d00c8925c916eac5907d9e9b3e9f9d21
Gai Tanaka <23jy0121@jec.ac.jp>
2024-10-23 15:55:18 +0900

    ローカルレポジトリでfor文を追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class Program {
   public static void main(String[] args){
     System.out.println("This is test commit 1");
     System.out.println("This is test commit 2");
+    for(int i=0; i<10; i++) {
+    	System.out.println("Local Commit");
+    }
   }
 }
