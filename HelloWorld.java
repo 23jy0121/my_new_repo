d2bd06a9daf56268c4e537bba4a9960881a3e3e0
23jy0121 <23jy0121@jec.ac.jp>
2024-10-23 15:56:22 +0900

    githubから変更したHelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class Program {
   public static void main(String[] args){
     System.out.println("This is test commit 1");
     System.out.println("This is test commit 2");
+    for　(int i=0; i<5; i++) {
+    	System.out.println("Local Commit");
+    }
   }
 }
