8712aab159a56d32ef1e77c293a9f36a5b13432f
Gai Tanaka <23jy0121@jec.ac.jp>
2024-10-23 16:01:22 +0900

    全角空白が入っていた部分を修正

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class Program {
   public static void main(String[] args){
     System.out.println("This is test commit 1");
     System.out.println("This is test commit 2");
-    for　(int i=0; i<5; i++) {
+    for (int i=0; i<5; i++) {
     	System.out.println("Local Commit");
     }
   }
